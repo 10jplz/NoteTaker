405c6bc2c6ecc1e930b6dda9d2bc46f2f6f42f6f
Jose Contreras <10jplz@gmail.com>
2021-03-16 17:29:14 -0600

    Final Commit

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,32 +1,117 @@
-//Linking our routes to our DATA 
 const fs = require("fs");
 const path = require("path");
-const data = require("../db/db.json");
-const database = path.join(__dirname, '..', 'db', 'db.json');
+var data = require("../db/db.json");
+var database = path.join(__dirname, '..', 'db', 'db.json');
 
+module.exports = function (app) {
 
-//ROUTING 
-module.exports = (app) => {
+    app.get("/api/notes", function (req, res) {
+        fs.readFile(database, "utf8", function (err, notes) {
+            if (err) throw err
+            else {
+                notes = JSON.parse(notes);
+                return res.json(notes);
+            }
+        });
+    });
+    app.get("/api/notes/:id", function (req, res) {
+        var chosen = req.params.id;
 
-    app.get('api/notes', (req, res) => {
-        let dbData = fs.readFileSync("db/db.json", "utf-8")
-      //  dbData = JSON.parse(dbData);
-        return res.json(dbData);
-    })
-
-
-    app.post('api/notes', (req, res) => {
-        let dbData = fs.readFileSync("db/db.json", "utf-8");
-        dbData = JSON.parse(dbData);
-        let newDBState = {
-            "title": req.body.title,
-            "text": req.body.text,
-            "id": Date.now(),
+        for (var i = 0; i < data.length; i++) {
+            if (chosen === data[i].id) {
+                return res.json(data[i])
+            }
         }
+    });
+
+    app.post("/api/notes", function (req, res) {
+        var note = req.body;
+        fs.readFile(database, "utf8", function (err, res) {
+            if (err) throw err
+            else {
+                data = JSON.parse(res)
+                data.push(note)
+
+                for (var i = 0; i < data.length; i++) {
+                    data[i].id = i.toString()
+                }
+
+                fs.writeFile(database, JSON.stringify(data), function (err) {
+                    if (err) throw err
+                    else {
+                        console.log("Your Note has been Saved")
+                    }
+                })
+            }
+        })
+
+        res.json(note)
+    });
+
+    app.delete("/api/notes/:id", function (req, res) {
+        var chosen = req.params.id;
+
+        fs.readFile(database, "utf8", function (err, notes) {
+
+            notes = JSON.parse(notes);
+
+            if (err) throw err
+            else {
+                for (var i = 0; i < data.length; i++) {
+                    if (chosen === data[i].id) {
+                        data.splice(i, 1);
+                    }
+                }
+
+                for (var i = 0; i < data.length; i++) {
+                    data[i].id = i.toString()
+                }
+
+                fs.writeFile(database, JSON.stringify(data), function (err) {
+                    if (err) throw err
+                    else {
+                        console.log("Note has been Updated")
+                        return res.json(notes);
+
+                    }
+                });
+            };
+
+        });
+    });
 
 
-        dbData.push(newDBState);
-        fs.writeFileSync(path.join(__dirname, "db/db.json"), JSON.stringify(dbData, null, 2));
-        res.send(true);
-    })
+    app.put("/api/notes/:id", function (req, res) {
+        var chosen = req.params.id;
+
+        fs.readFile(database, "utf8", function (err, notes) {
+
+            notes = JSON.parse(notes);
+
+            if (err) throw err
+            else {
+                data = data.map((note) => {
+                if(note.id === req.params.id){
+                    return {
+                        title: req.body.title,
+                        text: req.body.text,
+                        id: note.id
+                    }
+                }
+                });
+
+                fs.writeFile(database, JSON.stringify(data), function (err) {
+                    if (err) throw err
+                    else {
+                        console.log("Note has been Updated")
+                        return res.json(notes);
+
+                    }
+                });
+            };
+
+        });
+    });
 }
+
+ 
